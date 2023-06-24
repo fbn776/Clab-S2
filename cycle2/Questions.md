a62a6bd29cc0466f2ca0ebfc5241388f9fad8dd8
fbn776 <fbn776@gmail.com>
2023-06-24 13:06:34 +0530

    Added ex - 8

--- cycle2/Questions.md
+++ cycle2/Questions.md
@@ -2,8 +2,7 @@
 ### Questions
 
 8. Search for a particular number in a list of N numbers using linear search . Display
-the position of the number in the list if the number is present. Otherwise, display suit-
-able message.
+the position of the number in the list if the number is present. Otherwise, display suitable message.
 9. Display the binary equivalent of an unsigned decimal integer. `(using 1D array)`
 10. Sort N integers in ascending order using bubble sort algorithm.
 11. Search for a number in a sorted list of numbers using binary search .
---
+++ cycle2/ex-8.c
@@ -0,0 +1,31 @@
+/*
+Ex - 8
+Linear search
+29) Febin Nelson P, S2 CSE
+*/
+#include <stdio.h>
+
+void main() {
+    int numbers[30], N, i, key, found = 0, index;
+    printf("Enter N: ");
+    scanf("%d", &N);
+    printf("Enter numbers: ");
+
+    for(i=0; i<N;i++)
+        scanf(" %d", &numbers[i]);
+
+    printf("Enter number to search: ");
+    scanf(" %d", &key);
+
+    for(i=0; i<N; i++)
+        if(numbers[i] == key) {
+            found = 1;
+            index = i + 1;
+            break;
+        }
+
+    if(found)
+        printf("Found %d at index %d", key, index);
+    else
+        printf("Item not found");
+}

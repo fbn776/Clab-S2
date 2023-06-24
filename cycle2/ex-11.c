404317997e56bc96e386150a7534407ccd42d964
fbn776 <fbn776@gmail.com>
2023-06-24 13:53:38 +0530

    Added ex - 10

---
+++ cycle2/ex-11.c
@@ -0,0 +1,39 @@
+/*
+Ex - 11
+Binary number
+29) Febin Nelson P, S2 CSE
+*/
+#include <stdio.h>
+
+void main() {
+    int N, nums[30], key, i, top = 0, middle, bottom;
+    printf("Enter N: ");
+    scanf("%d", &N);
+    printf("Enter sorted numbers list: ");
+    for (i = 0; i < N; i++)
+        scanf(" %d", &nums[i]);
+
+    printf("Enter number to search: ");
+    scanf("%d", &key);
+
+    bottom = N - 1;
+
+    while(top < bottom) {
+        middle = top + (bottom - top) / 2;
+        printf("Middle is %d\n", nums[middle]);
+        if(nums[middle] < key) {
+            top = middle + 1;
+        }else if(nums[middle] > key) {
+            bottom = middle - 1;
+        }else {
+            top = bottom = middle;
+            break;
+        }
+    }
+
+    if(top == bottom) {
+        printf("Found at index %d\n", top + 1);
+    }else {
+        printf("Not found");
+    }
+}

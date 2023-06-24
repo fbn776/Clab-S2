02bd343ba76fa4ba2407e3eac0ec8f82cf21afa6
fbn776 <fbn776@gmail.com>
2023-06-24 13:27:07 +0530

    Added ex - 10

---
+++ cycle2/ex-10.c
@@ -0,0 +1,28 @@
+/*
+Ex - 10
+bubble sorting in ascending order
+29) Febin Nelson P, S2 CSE
+*/
+#include <stdio.h>
+
+void main() {
+    int N, nums[30], i, j;
+    printf("Enter N: ");
+    scanf("%d", &N);
+    printf("Enter numbers: ");
+    for (i = 0; i < N; i++)
+        scanf(" %d", &nums[i]);
+
+    for (i = 0; i < N; i++)
+        for(j=0; j < N-i-1; j++)
+            if(nums[j] < nums[j+1]) {
+                int temp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = temp;
+            }
+
+    for(i=0; i<N; i++)
+        printf("%d ", nums[i]);
+
+    printf("\n");
+}

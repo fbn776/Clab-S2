c4ff979fc880653af4d2a79ba5cb69f912918456
fbn776 <fbn776@gmail.com>
2023-06-24 11:36:46 +0530

    Added ex-1

---
+++ cycle1/Questions.md
@@ -0,0 +1,9 @@
+# Cycle 1
+### Questions
+1. Find the roots of a quadratic equation in the form `ax^2 + bx + c = 0`. `(if .. else)`
+2. Input a month number and display the number of days in that month. `(switch)`
+3. Find the GCD and LCM of two numbers. `(while / do while)`
+4. Find the largest and smallest of a set of N numbers. `(for)`
+5. Input a binary number and display the decimal equivalent of that number. `(while do .. while)`
+6. Display the first N prime numbers. `(nested loop)`
+7. Display the perfect numbers in first N counting numbers. `(nested loop)`
---
+++ cycle1/ex-1.c
@@ -0,0 +1,31 @@
+/*
+Ex - 1
+Roots of a quadratic equation.
+29) Febin Nelson P, S2 CSE
+*/
+#include <stdio.h>
+#include <math.h>
+
+void main() {
+    int a, b, c, desc;
+    float root1, root2;
+    printf("Enter a b and c: ");
+    scanf("%d %d %d", &a, &b, &c);
+    desc = b*b - 4*a*c;
+    if(desc > 0) {
+        root1 = (-b + sqrt(desc))/(2.0*a);
+        root2 = (-b - sqrt(desc))/(2.0*a);
+        printf("The roots are real and unique\n");
+        printf("x1 = %.2f\nx2 = %.2f\n", root1, root2);
+    }else if(desc == 0) {
+        root1 = -b / (2.0*a);
+        printf("The roots are real and equal\n");
+        printf("x = %.2f", root1);
+    }else {
+        float rp = -b / (2.0*a);
+        float ip = sqrt(-desc) / (2.0*a);
+        printf("The roots are imaginary\n");
+        printf("x1 = %0.2f + %0.2fi\n", rp, ip);
+        printf("x2 = %0.2f - %0.2fi\n", rp, ip);
+    }
+}

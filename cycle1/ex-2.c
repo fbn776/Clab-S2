12bbd42ac12597158c09f5647c6f3fb651364adf
fbn776 <fbn776@gmail.com>
2023-06-24 12:00:15 +0530

    Added ex-2

---
+++ cycle1/ex-2.c
@@ -0,0 +1,38 @@
+/*
+Ex - 2
+Number of days in a month
+29) Febin Nelson P, S2 CSE
+*/
+#include <stdio.h>
+
+int main() {
+    int month, year;
+    printf("Enter the month (1-12): ");
+    scanf("%d", &month);
+    if(month > 12 || month < 1) {
+        printf("Invalid month\n");
+        return 0;
+    }
+    switch (month) {
+        case 2:
+            printf("Enter the year: ");
+            scanf("%d", &year);
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                printf("no of days is %d\n", 29);
+            else
+                printf("no of days is %d\n", 28);
+            break;
+
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            printf("no of days is %d\n", 30);
+            break;
+
+        default:
+            printf("no of days is %d\n", 31);
+            break;
+    }
+    return 0;
+}

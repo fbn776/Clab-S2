720360f142c6b99dfe8bca92a6c4866941642f19
fbn776 <fbn776@gmail.com>
2023-07-11 01:07:50 +0530

    Updated 'ex-12.c'

--- cycle2/ex-12.c
+++ cycle2/ex-12.c
@@ -1,6 +1,10 @@
+/*
+ * Ex - 12
+ * Given two sets (mathematical set) of numbers A and B. Find A union B, A intersection B, A - B.
+ */
 #include <stdio.h>
 void main() {
-    int A[30], B[30], Union[60], Intrs[30], m, n, i, j, unIndex, inIndex = 0;
+    int A[30], B[30], Union[60], Intrs[30], Minus[30], m, n, i, j, unIndex, inIndex = 0, mIndex = 0, inA = 0;
 
     printf("Enter no of elements in A: ");
     scanf(" %d", &m);
@@ -30,12 +34,27 @@ void main() {
             Union[unIndex++] = B[i];
     }
 
+	//A intersection B
     for(i=0; i<m; i++)
         for(j=0; j<n; j++)
             if(A[i] == B[j]) {
                 Intrs[inIndex++] = A[i];
                 break;
             }
+
+	//A - B
+	for(i=0; i<m; i++) {
+		inA = 1;
+		for(j=0; j<n; j++) {
+			if(A[i] == B[j]) {
+				inA = 0;
+				break;
+			}
+		}
+		if(inA == 1) {
+			Minus[mIndex++] = A[i];
+		}
+	}
 
     printf("A union B is:\n");
     for(i=0; i<unIndex; i++)
@@ -46,4 +65,9 @@ void main() {
     for(i=0; i<inIndex; i++)
         printf("%d ", Intrs[i]);
     printf("\n");
+
+	printf("A - B is:\n");
+	for(i=0; i<mIndex; i++)
+		printf("%d ", Minus[i]);
+	printf("\n");
 }
